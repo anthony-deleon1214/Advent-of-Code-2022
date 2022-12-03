4595532321c689cde1d5210fd1ca40450b038635
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-03 14:02:04 -0500

    Working on first problem of Day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,8 @@
+// Importing File struct from standard library
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = File::open("puzzleInput.txt").expect("Requires puzzleInput.txt file");
+
+    let mut cal_vec: Vec<u32> = Vec::new();
 }
