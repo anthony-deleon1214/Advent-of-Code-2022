cf220a4b4b2406498b7017ecf675366f9f49dc84
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-03 17:24:16 -0500

    Restructuring into smaller functions

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,13 @@
 // Importing File struct from standard library
 use std::fs::File;
+use std::io::{self, Read};
 
 fn main() {
-    let puzzle_input = File::open("puzzleInput.txt").expect("Requires puzzleInput.txt file");
+    let mut calorie_list: Vec<i32> = Vec::new();
+}
 
-    let mut cal_vec: Vec<u32> = Vec::new();
+fn readFile() -> Result<String, io::Error> {
+    let puzzle_input: File = File::open("puzzleInput.txt").expect("Requires puzzleInput.txt file");
+
+
 }
