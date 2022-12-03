2846cda42408414fe6c3b75cfd4ca284a1070dc4
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-02 19:43:01 -0500

    Initial commit for day 1 problems

---
+++ day1/target/debug/deps/day1-0c55c63a349a70da.d
@@ -0,0 +1,5 @@
+c:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day1\target\debug\deps\day1-0c55c63a349a70da.rmeta: src\main.rs
+
+c:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day1\target\debug\deps\day1-0c55c63a349a70da.d: src\main.rs
+
+src\main.rs:
---
+++ day1/target/debug/deps/day1-cd662a75f3fb981f.d
@@ -0,0 +1,5 @@
+c:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day1\target\debug\deps\day1-cd662a75f3fb981f.rmeta: src\main.rs
+
+c:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day1\target\debug\deps\day1-cd662a75f3fb981f.d: src\main.rs
+
+src\main.rs:
