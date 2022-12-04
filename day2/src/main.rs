21fb9eb2bcc07cd010949ac08202e34a5a11bf7c
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-04 14:33:58 -0500

    Finishing first part of day 2 problem

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,17 +2,6 @@
 use std::fs;
 
 // Creating enums for opponent plays, my plays, and outcomes
-enum Opponent {
-    A(String),
-    B(String),
-    C(String),
-}
-
-enum Player {
-    X(String),
-    Y(String),
-    Z(String),
-}
 enum Outcome {
     Loss,
     Draw,
@@ -22,11 +11,54 @@ fn main() {
     let input: String = fs::read_to_string("src/input.txt")
         .expect("input.txt file is required");
 
-    
+    let mut score: u32 = 0;
+    let mut lines = input.lines();
+
+    // Loop through all lines of the input
+    loop {
+        let line = match lines.next() {
+            Some(line) => line,
+            None => break
+        };
+
+        let line = line.trim();
+        score += selection_score(&line);
+
+        let round_score = match check_outcome(&line) {
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+            Outcome::Win => 6,
+        };
+
+        score += round_score;
+    };
+
+    println!("The final score is {}", score)
 }
 
-fn selection_score(selection: Player<&str>)
+// Takes a line as a string slice and returns the proper selection score
+fn selection_score(line: &str) -> u32 {
+    if line.ends_with("X") {
+        return 1
+    } else if line.ends_with("Y") {
+        return 2
+    } else { // Only X, Y, or Z should be possible options given the input
+        return 3
+    }
+}
 
-fn check_outcome(line: &str) -> u32 {
-
+// 
+fn check_outcome(line: &str) -> Outcome {
+    match line {
+        "A X" => Outcome::Draw,
+        "A Y" => Outcome::Win,
+        "A Z" => Outcome::Loss,
+        "B X" => Outcome::Loss,
+        "B Y" => Outcome::Draw,
+        "B Z" => Outcome::Win,
+        "C X" => Outcome::Win,
+        "C Y" => Outcome::Loss,
+        "C Z" => Outcome::Draw,
+        _ => panic!("Impossible round found"),
+    }
 }
---
+++ day2/target/debug/deps/day2.d
@@ -0,0 +1,5 @@
+C:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day2\target\debug\deps\day2.exe: src\main.rs
+
+C:\Users\Anthony\Documents\Coding Projects\Advent-of-Code-2022\day2\target\debug\deps\day2.d: src\main.rs
+
+src\main.rs:
