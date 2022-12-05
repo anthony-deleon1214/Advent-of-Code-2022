e2fe0e1febb0beeb6bdf1f09260ea31b5568fd99
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-05 11:55:02 -0500

    Discarding original ideas for day 3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -22,30 +22,11 @@ fn main() {
         let comp_length = line_length/2;
         let first_half = &line[..comp_length];
         let second_half = &line[comp_length+1..];
-
-        compare_halves(first_half, second_half);
     }
 }
 
-fn compare_halves(first_half: &str, second_half: &str) {
-    // Bitwise AND to determine what values are shared between halves
-    let first_half_bytes = first_half.as_bytes();
-    let second_half_bytes = second_half.as_bytes();
-
-    let mut first_byte = first_half_bytes.iter();
-    let mut second_byte = second_half_bytes.iter();
-    let mut shared_values = Vec::new();
-
-    loop {
-        let first_result = match first_byte.next() {
-            Some(byte) => byte,
-            None => break,
-        };
-        let second_result = match second_byte.next() {
-            Some(byte) => byte,
-            None => break,
-        };
-
-        shared_values.push(first_result & second_result)
-    }
+fn create_bit_representation(compartment: &str) -> u64 {
+    // Creates a binary value based on the priority of the characters in the string slice
+    let mut str_iter = compartment.chars();
+    let bin_rep: u64 = 0;
 }
