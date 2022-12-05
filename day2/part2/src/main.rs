05a0a093b118f89bda3678db5fa60ad05c57b95e
Anthony Deleon <anthony.deleon1214@gmail.com>
2022-12-04 20:26:57 -0500

    Finishing part 2 of day 2

--- day2/part2/src/main.rs
+++ day2/part2/src/main.rs
@@ -1,4 +1,6 @@
 // Importing filesystem from standard library
+use std::fs;
+
 enum Outcome {
     Loss,
     Draw,
@@ -13,15 +15,62 @@ fn main() {
     let mut lines = input.lines();
     
     loop {
-        let line = match lines.next() {
+        let mut line = match lines.next() {
             Some(line) => line,
             None => break,
         };
 
+        line = line.trim();
+        let outcome = get_outcome(line);
+
+        score += match outcome {
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+            Outcome::Win => 6,
+        };
+        
+        score += selection_score(line, outcome);
     }
+
+    println!("The total score is {}", score)
 }
 
 // Takes a line from the puzzle input and determines player output and score
-fn selection_score(line: &str) -> u32 {
+fn selection_score(line: &str, outcome: Outcome) -> u32 {
+    let selection_score: u32;
+    if line.starts_with("A") {
+        selection_score = match outcome {
+            Outcome::Loss => 3,
+            Outcome::Draw => 1,
+            Outcome::Win => 2,
+        }
+    } else if line.starts_with("B") {
+        selection_score = match outcome {
+            Outcome::Loss => 1,
+            Outcome::Draw => 2,
+            Outcome::Win => 3,
+        }
+    } else if line.starts_with("C") {
+        selection_score = match outcome {
+            Outcome::Loss => 2,
+            Outcome::Draw => 3,
+            Outcome::Win => 1,
+        }
+    } else {
+        panic!("Invalid opponent selection")
+    };
 
+    selection_score
+}
+
+fn get_outcome(line: &str) -> Outcome {
+    if line.ends_with("X") {
+        return Outcome::Loss
+    } else if line.ends_with("Y") {
+        return Outcome::Draw
+    } else if line.ends_with("Z") {
+        return Outcome::Win
+    } else {
+        panic!("Impossible line ending found")
+    }
 }
