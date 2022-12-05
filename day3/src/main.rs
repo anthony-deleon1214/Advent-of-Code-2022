a673d9785359f955f92809b3133dc38ff92c8921
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-05 00:42:06 -0500

    Initial attempts at day 3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -20,19 +20,32 @@ fn main() {
         line = line.trim();
         let line_length = line.len();
         let comp_length = line_length/2;
-        let first_half = match line.get(..comp_length) {
-            Some(line) => line,
-            None => panic!("First half of line not found")
-        };
-        let second_half = match line.get(comp_length+1..) {
-            Some(line) => line,
-            None => panic!("Second half of line not found")
-        };
+        let first_half = &line[..comp_length];
+        let second_half = &line[comp_length+1..];
 
         compare_halves(first_half, second_half);
     }
 }
 
 fn compare_halves(first_half: &str, second_half: &str) {
-    
+    // Bitwise AND to determine what values are shared between halves
+    let first_half_bytes = first_half.as_bytes();
+    let second_half_bytes = second_half.as_bytes();
+
+    let mut first_byte = first_half_bytes.iter();
+    let mut second_byte = second_half_bytes.iter();
+    let mut shared_values = Vec::new();
+
+    loop {
+        let first_result = match first_byte.next() {
+            Some(byte) => byte,
+            None => break,
+        };
+        let second_result = match second_byte.next() {
+            Some(byte) => byte,
+            None => break,
+        };
+
+        shared_values.push(first_result & second_result)
+    }
 }
