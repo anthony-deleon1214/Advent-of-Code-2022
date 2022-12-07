d8d7d3e67dba1be3ff2b4dff005290fe44ccddad
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-07 14:27:04 -0500

    Beginning work on day 5 part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -10,5 +10,21 @@ fn part1() {
         .expect("Requires input.txt file");
 
     let mut lines = input.lines();
-    
+}
+
+fn load_crates(line: &str, mut stack_vec: Vec<Vec<&str>>) {
+    let mut char_count = 0;
+
+    let mut char_iter = line.chars();
+
+    loop {
+        let char = match char_iter.next() {
+            Some(char) => char,
+            None => break
+        };
+        
+        char_count += 1;
+        let target_vec = char_count/4;
+        
+    }
 }
