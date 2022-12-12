fcd1f3edb806c773c4f12e7b72e86f39094550da
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-11 19:41:52 -0500

    Initial work on day 7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,43 @@
+// Importing filesystem from standard library
+use std::fs;
+
+// Defining an enum for input lines
+enum TerminalOutput<'a> {
+    Command(&'a str),
+    Directory(&'a str),
+    Size(u32),
+}
+
 fn main() {
-    println!("Hello, world!");
+    part1()
+}
+
+fn part1() {
+    let input = fs::read_to_string("src/test.txt")
+        .expect("Requires input.txt file");
+
+    let mut lines = input.lines();
+    let mut cwd = "/";
+    
+    loop {
+        let line = match lines.next() {
+            Some(line) => line,
+            None => break,
+        };
+        let parsed_line = match parseOutput(line) {
+            TerminalOutput::Command => 
+        }
+    }
+}
+
+fn parseOutput(line: &str) -> TerminalOutput {
+    if line.starts_with('&') {
+        let parsed_command = line.strip_prefix('$').unwrap();
+        return TerminalOutput::Command(parsed_command)
+    } else if line.starts_with("dir") {
+        let directory_name = line.strip_prefix("dir").unwrap();
+        return TerminalOutput::Directory(directory_name);
+    }
+    let file_size:u32 = line.split(' ').next().unwrap().parse().expect("First part of line is not numeric");
+    TerminalOutput::Size(file_size)
 }
